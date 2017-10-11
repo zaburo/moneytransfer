6b43ce7ed386c85dec48771d0ef06ce190727b2d
François Moureau <moureauf@gmail.com>
2017-10-12 00:56:07 +0200

    Add POM

--- src/test/java/com/francoismoureau/moneytransfer/model/MoneyTransferVerticleTest.java
+++ src/test/java/com/francoismoureau/moneytransfer/model/MoneyTransferVerticleTest.java
@@ -21,64 +21,64 @@ import java.net.ServerSocket;
 @RunWith(VertxUnitRunner.class)
 public class MoneyTransferVerticleTest {
 
-  private Vertx vertx;
-  private Integer port;
+    private Vertx vertx;
+    private Integer port;
 
-  /**
-   * Before executing our test, let's deploy our verticle.
-   * <p/>
-   * This method instantiates a new Vertx and deploy the verticle. Then, it waits in the verticle has successfully
-   * completed its start sequence (thanks to `context.asyncAssertSuccess`).
-   *
-   * @param context the test context.
-   */
-  @Before
-  public void setUp(TestContext context) throws IOException {
-    vertx = Vertx.vertx();
+    /**
+     * Before executing our test, let's deploy our verticle.
+     * <p/>
+     * This method instantiates a new Vertx and deploy the verticle. Then, it waits in the verticle has successfully
+     * completed its start sequence (thanks to `context.asyncAssertSuccess`).
+     *
+     * @param context the test context.
+     */
+    @Before
+    public void setUp(TestContext context) throws IOException {
+        vertx = Vertx.vertx();
 
-    // Let's configure the verticle to listen on the 'test' port (randomly picked).
-    // We create deployment options and set the _configuration_ json object:
-    ServerSocket socket = new ServerSocket(0);
-    port = socket.getLocalPort();
-    socket.close();
+        // Let's configure the verticle to listen on the 'test' port (randomly picked).
+        // We create deployment options and set the _configuration_ json object:
+        ServerSocket socket = new ServerSocket(0);
+        port = socket.getLocalPort();
+        socket.close();
 
-    DeploymentOptions options = new DeploymentOptions()
-        .setConfig(new JsonObject().put("http.port", port)
-        );
+        DeploymentOptions options = new DeploymentOptions()
+                .setConfig(new JsonObject().put("http.port", port)
+                );
 
-    // We pass the options as the second parameter of the deployVerticle method.
-    vertx.deployVerticle(MoneyTransferVerticle.class.getName(), options, context.asyncAssertSuccess());
-  }
+        // We pass the options as the second parameter of the deployVerticle method.
+        vertx.deployVerticle(MoneyTransferVerticle.class.getName(), options, context.asyncAssertSuccess());
+    }
 
-  /**
-   * This method, called after our test, just cleanup everything by closing the vert.x instance
-   *
-   * @param context the test context
-   */
-  @After
-  public void tearDown(TestContext context) {
-    vertx.close(context.asyncAssertSuccess());
-  }
+    /**
+     * This method, called after our test, just cleanup everything by closing the vert.x instance
+     *
+     * @param context the test context
+     */
+    @After
+    public void tearDown(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
 
-  /**
-   * Let's ensure that our application behaves correctly.
-   *
-   * @param context the test context
-   */
-  @Test
-  public void testMyApplication(TestContext context) {
-    // This test is asynchronous, so get an async handler to inform the test when we are done.
-    final Async async = context.async();
+    /**
+     * Let's ensure that our application behaves correctly.
+     *
+     * @param context the test context
+     */
+    @Test
+    public void testMyApplication(TestContext context) {
+        // This test is asynchronous, so get an async handler to inform the test when we are done.
+        final Async async = context.async();
 
-    // We create a HTTP client and query our application. When we get the response we check it contains the 'Hello'
-    // message. Then, we call the `complete` method on the async handler to declare this async (and here the test) done.
-    // Notice that the assertions are made on the 'context' object and are not Junit assert. This ways it manage the
-    // async aspect of the test the right way.
-    vertx.createHttpClient().getNow(port, "localhost", "/", response -> {
-      response.handler(body -> {
-        context.assertTrue(body.toString().contains("MoneyTransfer"));
-        async.complete();
-      });
-    });
-  }
+        // We create a HTTP client and query our application. When we get the response we check it contains the 'Hello'
+        // message. Then, we call the `complete` method on the async handler to declare this async (and here the test) done.
+        // Notice that the assertions are made on the 'context' object and are not Junit assert. This ways it manage the
+        // async aspect of the test the right way.
+        vertx.createHttpClient().getNow(port, "localhost", "/", response -> {
+            response.handler(body -> {
+                context.assertTrue(body.toString().contains("MoneyTransfer"));
+                async.complete();
+            });
+        });
+    }
 }
